62cdeda5a2ffd3c58b5a4f7e12c8b13d48ccc059
Vikas Kumar <negi.vikash925@gmail.com>
2021-05-23 13:44:04 +0530

    Fix static method issue

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -9,29 +9,25 @@ class Parser:
     def valid_extension(self, extension):
         return extension in self.extensions
 
-    @staticmethod
-    def parse(path, source, dest):
+    def parse(self, path, source, dest):
         raise NotImplementedError
 
-    @staticmethod
-    def read(path):
+    def read(self, path):
         with open(path, 'rt') as file:
             return file.read()
 
-    @staticmethod
-    def write(path, dest, content, ext=".html"):
+    def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
 
         with open(full_path, 'wt') as file:
             file.write(content)
 
-    @staticmethod
-    def copy(path, source,dest):
+    def copy(self, path, source,dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
 
 class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
-    def parse(path, source, dest):
+    def parse(self, path, source, dest):
         Parser.copy(path, source, dest)
