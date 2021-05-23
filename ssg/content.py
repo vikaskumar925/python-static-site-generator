4a75cebe3284d347f49f79e2f66a4d37d734ad38
Vikas Kumar <negi.vikash925@gmail.com>
2021-05-23 14:22:37 +0530

    Fix regex issue

--- ssg/content.py
+++ ssg/content.py
@@ -4,7 +4,7 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
+    __delimeter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
 
     @classmethod
