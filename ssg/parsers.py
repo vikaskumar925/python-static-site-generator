07df87028449a780df7081887be13a1af87df489
Vikas Kumar <negi.vikash925@gmail.com>
2021-05-23 13:47:34 +0530

    Fix argument type issue

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -9,17 +9,17 @@ class Parser:
     def valid_extension(self, extension):
         return extension in self.extensions
 
-    def parse(self, path, source, dest):
+    def parse(self, path:Path, source:Path, dest:Path):
         raise NotImplementedError
 
     def read(self, path):
-        with open(path, 'rt') as file:
+        with open(path, "r") as file:
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
 
-        with open(full_path, 'wt') as file:
+        with open(full_path, 'w') as file:
             file.write(content)
 
     def copy(self, path, source,dest):
@@ -29,5 +29,5 @@ class Parser:
 class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
-    def parse(self, path, source, dest):
+    def parse(self, path:Path, source:Path, dest:Path):
         Parser.copy(path, source, dest)
